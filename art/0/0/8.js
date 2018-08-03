8483e112c6367c7bd5c6ca0ba81e8a3a390e12e2
George Gally <robopop@radarboy.com>
2018-08-03 14:31:22 +0200

    bank 2

--- art/0/0/8.js
+++ art/0/0/8.js
@@ -36,16 +36,13 @@ rbvj = function () {
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  // position and point the camera to the center of the scene
   camera.position.x = -20;
   camera.position.y = 30;
   camera.position.z = 40;
   camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
-
   var date = new Date();
 
-  // add subtle ambient lighting
   var ambientLight = new THREE.AmbientLight( 0x444444 );
   scene.add( ambientLight );
 
